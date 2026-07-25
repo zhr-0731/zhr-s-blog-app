0f0169211d124a2bb47631d8d0737aa89d5c76a5
zhr-0731 <zhr_898@qq.com>
2026-07-25 20:56:43 +0800

    new: 新建文件 Type.kt
    Signed-off-by: zhr-0731 <zhr_898@qq.com>

---
+++ app/src/main/java/com/zhr/blog/ui/theme/Type.kt
@@ -0,0 +1,12 @@
+package com.zhr.blog.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val Typography = Typography(
+    titleMedium = TextStyle(fontWeight = FontWeight.Medium, fontSize = 18.sp),
+    bodyMedium = TextStyle(fontWeight = FontWeight.Normal, fontSize = 14.sp),
+    bodySmall = TextStyle(fontWeight = FontWeight.Light, fontSize = 12.sp)
+)
