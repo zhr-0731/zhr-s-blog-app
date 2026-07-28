2d850cfdabdab3f0e893ca72eb0f0388794678ae
zhr-0731 <220207206+zhr-0731@users.noreply.github.com>
2026-07-28 23:02:13 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,10 +19,9 @@ android {
         buildConfigField("String", "TIMESTAMP", "\"${System.currentTimeMillis()}\"")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-        // 为 AndroidManifest.xml 中的占位符提供默认值
         manifestPlaceholders["versionName"] = versionName
         manifestPlaceholders["versionCode"] = versionCode.toString()
-        manifestPlaceholders["buildType"] = "debug" // 默认值，会被 buildTypes 覆盖
+        manifestPlaceholders["buildType"] = "debug"
     }
 
     buildTypes {
@@ -32,13 +31,10 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            manifestPlaceholders["buildType"] = "release"
         }
         debug {
             versionNameSuffix = "-Beta"
             applicationIdSuffix = ".debug"
-            manifestPlaceholders["versionName"] = versionName + versionNameSuffix
-            manifestPlaceholders["buildType"] = "debug"
         }
     }
 
